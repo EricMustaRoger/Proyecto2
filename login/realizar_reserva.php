762e1b87c809f1b49434865b12bdbd9a6ae50353
Musta <8664.joan23@fje.edu>
2016-11-07 19:47:12 +0100

    realizarreserva222

--- login/realizar_reserva.php
+++ login/realizar_reserva.php
@@ -1,66 +1,32 @@
 <?php session_start();
 
-		//realizamos la conexión
-	$conexion = mysqli_connect('localhost', 'root', '', 'bd_cromo');
-
-		//le decimos a la conexión que los datos los devuelva diréctamente en utf8, así no hay que usar htmlentities
-		$acentos = mysqli_query($conexion, "SET NAMES 'utf8'");
-
-		if (!$conexion) {
-		    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-		    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
-		    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
-		    exit;
-		}
-
-		extract($_REQUEST);
-		if(isset($_POST['submit'])){//to run PHP script on submit
-		if(!empty($_POST['reservar'])){
-		// Loop to store and display values of individual checked checkbox.
-		foreach($_POST['reservar'] as $selected){
-		echo $selected."</br>";
-		}
-		}
-		}
-
-
-		if (isset($_POST['reservar'])){
-		echo $_POST['reservar']; // Displays value of checked checkbox.
-		}   
-
-		$sql = "UPDATE `tbl_recursos` SET `rec_disp` = b'0' WHERE `tbl_recursos`.`rec_id` = $reservar;";
-		//UPDATE `tbl_recursos` SET `rec_disp` = b'0' WHERE `tbl_recursos`.`rec_id` = 4;
-
-		//echo $sql."---------";
-		
-
-
-
-		$actualizar_recurso = mysqli_query($conexion, $sql);
-
-	
-
-		
-		?>
-
-		<?php
-		//----------------inserts
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "bd_cromo";
-
-
-
-//realizamos la conexión
-	$conexion = mysqli_connect('localhost', 'root', '', 'bd_cromo');
-	$data = date("Y") . date("m") . date("d") . date("G") . date("i") . date("s");
+if (isset($_SESSION['usu_name'])) {
+	$rec_id = $_GET['rec_id'];
+	$comentario = 'inc_comentario'.$rec_id;
+	$inc_comentario = filter_var(strtolower($_GET[$comentario]), FILTER_SANITIZE_STRING);
 	$usu_name = $_SESSION['usu_name'];
- 	$res_fechaini = $data;
-	$sql = "INSERT INTO tbl_reserva (rec_id, usu_id, res_fechaini)
-	VALUES ($reservar, '$usu_name', $data)";
+	$conexion = new PDO('mysql:host=localhost;dbname=bd_cromo','root','');
+
+	$statement = $conexion->prepare('SELECT * FROM tbl_usuarios WHERE usu_name = :usu_name');
+	$statement->execute(array(
+		':usu_name' => $usu_name
+	));
+	$resultados = $statement->fetchAll();
+	foreach ($resultados as $recursos) {
+			$usu_id = $recursos['usu_id'];
+			// echo "<tr><td>" . $recursos['rec_id'] . "</td><td>" . $recursos['rec_name'] . "</td><td>" . $recursos['rec_tipo'] . "</td><td>" . $recursos['rec_disp'] . "</td><td>" . $recursos['rec_foto'] . "</td></tr>";
+	}
+	$statement = $conexion->prepare('INSERT INTO `tbl_incidencias` (`inc_id`, `usu_id`, `rec_id`, `inc_comentario`) VALUES (NULL, :usu_id, :rec_id, :inc_comentario);');
+	$statement->execute(array(
+		':usu_id' => $usu_id,
+		':rec_id' => $rec_id,
+		':inc_comentario' => $inc_comentario
+	));
+	header('Location:incidencias.php');
+} else {
+	header('Location: login.php');
+}
 
 
-	$conexion->close();
-	header('location: portfolio.php');
+
 ?>
